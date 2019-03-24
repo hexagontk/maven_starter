b85581f08d3a9f32b50ceaf8834e3db8ace58014
Juanjo Aguililla <juanjoaguililla@gmail.com>
2019-03-24 13:19:52 +0100

    Upgrade Hexagon version

--- src/main/kotlin/MavenStarter.kt
+++ src/main/kotlin/MavenStarter.kt
@@ -2,24 +2,28 @@ package org.example
 
 import java.time.LocalDateTime.now
 
-import com.hexagonkt.server.*
-import com.hexagonkt.server.jetty.JettyServletAdapter
-import com.hexagonkt.settings.SettingsManager
+import com.hexagonkt.http.httpDate
+import com.hexagonkt.http.server.*
+import com.hexagonkt.http.server.jetty.JettyServletAdapter
+import com.hexagonkt.injection.InjectionManager.bindObject
 
-val server: Server = server(JettyServletAdapter(), SettingsManager.settings) {
-    before {
-        response.addHeader("Server", "Servlet/3.1")
-        response.addHeader("Transfer-Encoding", "chunked")
-        response.addHeader("Date", httpDate(now()))
+val server: Server by lazy {
+    Server {
+        before {
+            response.setHeader("Server", "Servlet/3.1")
+            response.setHeader("Transfer-Encoding", "chunked")
+            response.setHeader("Date", httpDate(now()))
+        }
+
+        get("/text") { ok("Hello, World!", "text/plain") }
     }
-
-    get("/text") { ok("Hello, World!", "text/plain") }
 }
 
 /**
  * Start the service from the command line.
  */
 fun main() {
-    server.run()
+    bindObject<ServerPort>(JettyServletAdapter())
+    server.start()
 }
 
