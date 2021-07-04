9e66d7ad8305fcdc16ccc55028142525936293b9
jaguililla <juanjoaguililla@gmail.com>
2021-07-04 17:39:06 +0200

    Improve template

--- src/main/kotlin/MavenStarter.kt
+++ src/main/kotlin/MavenStarter.kt
@@ -1,25 +1,25 @@
 package org.example
 
-import com.hexagonkt.logging.logger
 import com.hexagonkt.http.server.*
 import com.hexagonkt.http.server.jetty.JettyServletAdapter
 import com.hexagonkt.injection.InjectionManager
 
-internal val injector: InjectionManager = InjectionManager.apply {
-    module.bind<ServerPort>(JettyServletAdapter())
-}
+internal val server: Server by lazy {
+    Server {
+        before {
+            response.headers["Server"] = "Servlet/3.1"
+        }
 
-internal val server: Server = Server {
-    before {
-        response.headers["Server"] = "Servlet/3.1"
-    }
-
-    get("/text") {
-        ok("Hello, World!", "text/plain")
+        get("/text") {
+            ok("Hello, World!", "text/plain")
+        }
     }
 }
 
 internal fun main() {
-    logger.info { injector }
+    InjectionManager.apply {
+        module.bind<ServerPort>(JettyServletAdapter())
+    }
+
     server.start()
 }
