32819beb3758f4ddfc6d207a97e3ca53e20db682
jaguililla <juanjoaguililla@gmail.com>
2022-05-24 21:47:08 +0200

    Update Hexagon version

--- src/main/kotlin/MavenStarter.kt
+++ src/main/kotlin/MavenStarter.kt
@@ -5,12 +5,13 @@ import com.hexagonkt.http.server.jetty.JettyServletAdapter
 import com.hexagonkt.core.logging.LoggingManager
 import com.hexagonkt.core.media.TextMedia.PLAIN
 import com.hexagonkt.http.model.ContentType
+import com.hexagonkt.http.model.Header
 import com.hexagonkt.logging.slf4j.jul.Slf4jJulLoggingAdapter
 
 internal val server: HttpServer by lazy {
     HttpServer(JettyServletAdapter()) {
         on("*") {
-            send(headers = response.headers + ("server" to "Servlet/3.1"))
+            send(headers = response.headers + Header("server", "Servlet/3.1"))
         }
 
         get("/text") {
